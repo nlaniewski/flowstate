3efe2f0897a61eb09ca2d0fc40ca5bb922b593f4
nlaniewski <nathan_laniewski@urmc.rochester.edu>
2026-04-03 15:56:21 -0400

    renamed select functions using underscore; kde/contour selection

--- R/flowstate_utils.R
+++ R/flowstate_utils.R
@@ -186,7 +186,7 @@ check.keyword <- function(fcs.file.paths, keyword = NULL, value = NULL){
 #' )
 #'
 #' #UPDATES BY REFERENCE -- adds a new column named 'select.nonsaturating'
-#' select.nonsaturating(fs)
+#' select_nonsaturating(fs)
 #' fs$data[, .N, by = select.nonsaturating]
 #'
 #' #visualize
@@ -202,7 +202,7 @@ check.keyword <- function(fcs.file.paths, keyword = NULL, value = NULL){
 #' #NULL it out
 #' fs$data[,'select.nonsaturating' := NULL]
 #'
-select.nonsaturating <- function(flowstate){
+select_nonsaturating <- function(flowstate){
   ## scatter and raw/unmixed fluorescence (linear values);
   ## '$PnR/n1' value to get detector upper limit
   type.string <- c(
@@ -260,7 +260,7 @@ select.nonsaturating <- function(flowstate){
 #' flowstate.transform(fs)
 #'
 #' #UPDATES BY REFERENCE -- adds a new column named 'select.quantile'
-#' select.quantile(fs, probs = c(0.0005, 0.9995))
+#' select_quantile(fs, probs = c(0.0005, 0.9995))
 #' fs$data[, .N, by = select.quantile]
 #'
 #' #visualize
@@ -276,7 +276,7 @@ select.nonsaturating <- function(flowstate){
 #' #NULL it out
 #' fs$data[,'select.quantile' := NULL]
 #'
-select.quantile <- function(flowstate, probs = c(0.0005, 0.9995)){
+select_quantile <- function(flowstate, probs = c(0.0005, 0.9995)){
   ## scatter and raw/unmixed fluorescence
   type.string <- c(
     sprintf("%s_Scatter", c("Forward", "Side")),
@@ -313,3 +313,124 @@ select.quantile <- function(flowstate, probs = c(0.0005, 0.9995)){
 }
 
 cosine.similarity <- function(x,y){crossprod(x, y) / sqrt(crossprod(x) * crossprod(y))}
+
+kde.contour <- function(x, y, bandwidth.adjust = NULL, grid.points = 100, prob = 0.95){
+  ## pre-calculate bandwidth
+  h <- sapply(list(x,y), function(j) MASS::bandwidth.nrd(j))
+  ## adjust bandwidth
+  if(!is.null(bandwidth.adjust)){
+    h <- h * bandwidth.adjust
+  }
+  ## kernel density estimate
+  dens <- MASS::kde2d(x = x, y = y, h = h, n = grid.points)
+  ## major contours
+  levels <- stats::quantile(dens$z, probs = prob)
+  ## major contours -- lines
+  cl <- grDevices::contourLines(dens, levels = levels)
+  ## major contour -- line
+  cl <- cl[[which.max(sapply(cl, function(i) length(i$x)))]]
+}
+## SpectroFlo Reference Group-specific function
+select_scatter.population.contour <- function(
+    flowstate,
+    population.marker,
+    top.N.percent = 10,
+    bandwidth.adjust = 2,
+    grid.points = 100,
+    prob = 0.975
+){
+  ## alias
+  alias <- merge(
+    x = flowstate$parameters[, j = .(N, TYPE)],
+    y = alias_dt(flowstate),
+    sort = F
+  )
+  ## relevant variables
+  #cols.scatter <- sort(alias[grep("Scatter", TYPE)][['alias']])
+  cols.scatter <- c('FSC_A', 'SSC_A')
+  cols.detector <- alias[TYPE == "Raw_Fluorescence"][['alias']]
+  cols.by <- flowstate$data[, names(.SD), .SDcols = is.factor]
+  proj <- factor(unique(flowstate$keywords[['$PROJ']]))
+  ## if there are any bead controls, add to population marker;
+  ## any single fluor bead control should work...
+  if(flowstate$keywords[, "Beads" %in% levels(tissue.type)]){
+    ## take the first non-Unstained fluor bead control
+    bead.ctrl <- flowstate$keywords[
+      i = tissue.type == "Beads" & !grepl("Unstained", sample.id)
+    ][1, as.character(sample.id)]
+    population.marker <- c(population.marker, beads = bead.ctrl)
+  }
+  ##
+  contour.bound <- flowstate$data[
+    i = sample.id %in% population.marker,
+    j = {
+      ## mean of sorted vector to find peak detector
+      means.detector <- sapply(cols.detector,function(j){
+        mean(sort(.SD[[j]],decreasing = T)[1:100])
+      })
+      ## peak detector
+      detector <- names(which.max(means.detector))
+      ## top.N expressing events
+      top.N <- ceiling(.N*(top.N.percent/100))
+      ## index of ordered peak detector vector -- top expressing events
+      i <- order(.SD[[detector]],decreasing = T)[1:top.N]
+      ## the index allows a more discrete identification of scatter-based contour/landmark
+      cl <- kde.contour(
+        x = FSC_A[i],
+        y = SSC_A[i],
+        bandwidth.adjust = bandwidth.adjust,
+        grid.points = grid.points,
+        prob = prob
+      )
+      ## return population label(s) and major contour line (x, y)
+      c(
+        population =  names(which(population.marker == .BY$sample.id)),
+        cl[c('x', 'y')]
+      )
+    },
+    .SDcols = c(cols.detector,cols.scatter),
+    by = cols.by
+  ][,population := factor(population)][]
+  ## apply tissue.type/population-specific contour bounds to all relevant data (rows)
+  ## adds a population label and a logical for selection
+  for(.population in contour.bound[, levels(population)]){
+    .type <- ifelse(.population == 'beads', "Beads", "Cells")
+    flowstate$data[
+      i = tissue.type == .type,
+      j = c('population', 'select.contour') := {
+        cl <- sapply(c('x', 'y'), function(i){
+          contour.bound[population == .population][[i]]}, simplify = F)
+        res <- as.logical(sp::point.in.polygon(FSC_A, SSC_A, cl$x, cl$y))
+        list(.population, res)
+      }
+    ][]
+  }
+  ##
+  invisible(flowstate)
+}
+## SpectroFlo Reference Group-specific function
+select_contour.singlets <- function(
+    flowstate,
+    scatter.singlets = c('FSC', 'SSC'),
+    n.adjust = 0.2,
+    prob = 0.90
+)
+{
+  pulses <- paste(match.arg(scatter.singlets), c('H', 'W'), sep = "_")
+  flowstate$data[
+    ,
+    j = select.contour := {
+      .i <- sample(.N,floor(.N*n.adjust))
+      cl <- kde.contour(
+        x = .SD[[pulses[1]]][.i],
+        y = .SD[[pulses[2]]][.i],
+        prob = prob
+      )
+      in_contour <- as.logical(sp::point.in.polygon(.SD[[pulses[1]]], .SD[[pulses[2]]], cl$x, cl$y))
+    },
+    .SDcols = pulses,
+    by = population
+  ]
+  ##
+  invisible(flowstate)
+}

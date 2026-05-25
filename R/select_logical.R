08fa9040b194a23bd9ca19aabc5d5d6ac3b1847a
nlaniewski <nathan_laniewski@urmc.rochester.edu>
2026-05-24 20:02:13 -0400

    by sample.id

--- R/select_logical.R
+++ R/select_logical.R
@@ -205,7 +205,8 @@ select_singlets <- function(flowstate, quantiles = c(0.85, 0.95)){
     j = select.singlets := {
       res <- FSC_A/FSC_H
       res < stats::quantile(res, probs = quantiles[1])
-    }
+    },
+    by = sample.id
   ]
   ## remove doublets -- side scatter geometry; indexed against the initial selector
   ## selects events (collinear) based on quantile [2]
@@ -214,7 +215,8 @@ select_singlets <- function(flowstate, quantiles = c(0.85, 0.95)){
     j = select.singlets := {
       res <- SSC_A/SSC_H
       res < stats::quantile(res, probs = quantiles[2])
-    }
+    },
+    by = sample.id
   ]
   ##
   invisible(flowstate)

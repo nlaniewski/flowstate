85949b93ef934d5316db68c70ffcb2a9703e3fa7
nlaniewski <nathan_laniewski@urmc.rochester.edu>
2026-07-19 11:18:11 -0400

    updated globals

--- R/globals.R
+++ R/globals.R
@@ -3,15 +3,16 @@
 # dput(unlist(strsplit(trimws(utils::readClipboard())," ")))
 # dput(sort(c(...)))
 # c(...) |> sort() |> dput()
-utils::globalVariables(
+res <- utils::globalVariables(
   c(
-    "$CYT", "$TOT", ".", "..keywords.to.add", "FSC_A", "FSC_H", "N", "N.alias",
-    "R", "S", "S.alias", "SSC_A", "SSC_H", "TYPE", "V1", "alias",
-    "barcode", "barcode.alias", "barcode.censor", "combn.drop", "detector.peak",
-    "detector.select", "id", "j", "j.match", "j.match.parameters.to.data",
-    "node_barcode", "nodes.assigned", "ord", "sample.id", "scatter.select",
-    "select.nonsaturating", "select.population", "select.quantile",
-    "select.singlets", "totals", "value", "value.x", "value.y", "variable",
-    "variable.x", "variable.y"
+    "$CYT", "$TOT", ".", "..keywords.to.add", "alias", "barcode",
+    "barcode.alias", "barcode.censor", "combn.drop", "detector.peak",
+    "detector.select", "fsc.a", "fsc.h", "FSC_A", "FSC_H", "id",
+    "j", "j.match", "j.match.parameters.to.data", "kw", "N", "N.alias",
+    "node_barcode", "nodes.assigned", "ord", "R", "S", "S.alias",
+    "sample.id", "scatter.select", "select.nonsaturating", "select.population",
+    "select.quantile", "select.singlets", "ssc.a", "ssc.h", "SSC_A",
+    "SSC_H", "totals", "TYPE", "V1", "value", "value.x", "value.y",
+    "variable", "variable.x", "variable.y"
   )
 )
